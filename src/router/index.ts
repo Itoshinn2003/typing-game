862e61e76ce9a5f659d475aa9a3c4976cb94540e
itoshinn <shinn2003@au.com>
2024-09-14 17:59:53 +0900

    router/index.tsの設定（仮)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue';
-import About from '../views/Play.vue';
+import Play from '../views/Play.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/play',
+        name: 'Play',
+        component: Play
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
