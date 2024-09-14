a821b81be91bae0ae6ccaa1e565f02c009f422b4
itoshinn <shinn2003@au.com>
2024-09-14 18:53:20 +0900

    必要なファイル/フォルダ作成と色々試し。 App.vueからrouterつながった

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import './styles/common.css'
 import App from './App.vue'
+import router from './router';
+import { createPinia } from 'pinia';
 
 
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// import HelloWorld from './components/HelloWorld.vue'
+function a() {
+    console.log('a');
+}
+</script>
+
+<template>
+  <p class="fire">Hello World!</p>
+</template>
+
